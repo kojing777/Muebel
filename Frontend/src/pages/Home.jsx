cc793809ee3951177b53bf9279c27d3f10085a06
kojing777 <kojingmoktan92@gmail.com>
2025-06-20 21:18:18 +0545

    meubel

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
-import Category from '../components/Category'
 import TopPicks from '../components/TopPicks'
 import NewArrivels from '../components/NewArrivels'
 import Blogs from '../components/Blogs'
@@ -13,7 +12,6 @@ const Home = () => {
     <div>
         <Navbar/>
         <Hero/>
-        <Category/>
         <TopPicks/>
         <NewArrivels/>
         <Blogs/>
